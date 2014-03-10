a2861b0c13c89a5a73e310d99dfb9206b9d90072
Dima <dimliang95@gmail.com>
2014-03-10 21:05:49 +0400

    Categories.cpp created

---
+++ Categories.cpp
@@ -0,0 +1,29 @@
+#include "Categories.h"
+
+using namespace std;
+
+void Categories::add (Category* ptr) {
+	if (ptr != NULL) {
+        objects.push_back(ptr);
+	} else {
+		// notification for user or provoking class
+    }
+
+}
+
+Category* Categories::operator[] (const int& number) {
+		for (iterator it = objects.begin(); it != objects.end(); it ++) {
+		if ((*it)->getNumber() == number) {
+			return *it;
+		}
+	}
+	return NULL;
+}
+
+Categories::iterator Categories::begin(){
+	return objects.begin();
+}
+
+Categories::iterator Categories::end(){
+	return objects.end();
+}
